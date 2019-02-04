6fdc3a9b47db2748de806c8755acf2cc789382ea
Harsha MS <harshams@Gigskys-MacBook-Pro.local>
2019-02-04 10:27:14 +0530

    Initial commit

---
+++ src/main/java/com/test/controller/ViewController.java
@@ -0,0 +1,16 @@
+package com.test.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewController {
+
+    @RequestMapping(value="hello",method = RequestMethod.GET)
+    public String hello(Model model){
+        model.addAttribute("message","Hello There");
+        return "hello";
+    }
+}
